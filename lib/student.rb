b0e0b749b0dec927c271edb3fe94ce0de90a5d5f
Laura Berge <lauraeberge@gmail.com>
2019-09-21 19:56:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,11 +34,27 @@ class Student
   end
   
   def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE grade = 9
+      SQL
     
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def self.all_students_below_12th_grade
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE grade < 12
+      SQL
     
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def self.first_X_students_in_grade_10
